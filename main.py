4c594537137dcc62045399aa5b2b6614b626d125
zkan <kan@odds.team>
2026-09-11 06:30:44 +0700

    Create a simple agent

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from langchain.agents import create_agent
+
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+
 def main():
-    print("Hello from hello-langchain!")
+    agent = create_agent(
+        model="ollama:qwen3.5:4b",
+        tools=[get_weather],
+        system_prompt="You are a helpful assistant",
+    )
+
+    result = agent.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "What's the weather in San Francisco?"
+                }
+            ]
+        }
+    )
+    # print(result)
+    print(result["messages"][-1].content_blocks)
 
 
 if __name__ == "__main__":
