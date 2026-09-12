b0f74e755ecad67f8f26043e9bca359a00caffc4
zkan <kan@odds.team>
2026-09-12 07:18:31 +0700

    Add decorator

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from langchain.agents import create_agent
+from langchain.tools import tool
 
 
+@tool
 def get_weather(city: str) -> str:
     """Get weather for a given city."""
     return f"It's always sunny in {city}!"
